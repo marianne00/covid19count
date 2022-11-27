c2bf000eded64d0e08ba62d052e089042180b750
Marianne de Asis <marianne@moustacherepublic.com>
2022-11-27 22:53:31 +0800

    Updates

---
+++ precache-manifest.be6df01912530ec33d4e46d30caa2fef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b8394efabd4019b7729f580670cf78",
+    "url": "/covid19count/index.html"
+  },
+  {
+    "revision": "9701fc747da1012d1ac7",
+    "url": "/covid19count/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "e4e764d8c74cba64158b",
+    "url": "/covid19count/static/css/main.67ee785f.chunk.css"
+  },
+  {
+    "revision": "9701fc747da1012d1ac7",
+    "url": "/covid19count/static/js/2.2be15b6e.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/covid19count/static/js/2.2be15b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e764d8c74cba64158b",
+    "url": "/covid19count/static/js/main.cb4661eb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/covid19count/static/js/main.cb4661eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e146b384a578e3fb928",
+    "url": "/covid19count/static/js/runtime-main.c4ed9d1e.js"
+  },
+  {
+    "revision": "d38ddfbd1061d20d165fc60bb11a9890",
+    "url": "/covid19count/static/media/virus.d38ddfbd.svg"
+  }
+]);
